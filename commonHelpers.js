63ce1230dcf9cc880cc06f66b491e627dbe2acfb
Terrad77 <140712108+Terrad77@users.noreply.github.com>
2023-12-29 19:48:22 +0000

    Deploying to gh-pages from @ Terrad77/goit-js-hw-11@4014a32f469d8b43a7fa7f0cf5ba4b980b996e69 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,13 +1,15 @@
-import{i as l,s as g}from"./assets/vendor-87ec4f81.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&r(i)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();const h=document.querySelector(".form"),L=document.querySelector("#input");h.addEventListener("submit",s=>{s.preventDefault();let n=`https://pixabay.com/api/?key=41494285-2be0c6d487dc7750955372a82&q=${L.value.trim()}&image_type=photo&orientation=horizontal&safesearch=true`;const r=document.querySelector(".gallery");r.innerHTML="",fetch(n).then(e=>{if(!e.ok)throw new Error(e.status);return e.json()}).then(e=>{if(e.hits.length===0)throw l.show({message:"Sorry, there are no images matching your search query. Please try again!",color:"red",position:"topCenter"});const t=e.hits.reduce((a,{webformatURL:c,largeImageURL:u,tags:m,likes:p,views:d,comments:f,downloads:y})=>a+`<li class="gallery-item">
-          <a class="gallery-link" href="${u}">
+import{i as l,s as L}from"./assets/vendor-87ec4f81.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))t(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const s of r.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&t(s)}).observe(document,{childList:!0,subtree:!0});function a(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerpolicy&&(r.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?r.credentials="include":e.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function t(e){if(e.ep)return;e.ep=!0;const r=a(e);fetch(e.href,r)}})();const w=document.querySelector(".form"),c=document.querySelector("#input"),u=document.querySelector(".gallery"),n=document.querySelector(".loader");n.style.display="none";w.addEventListener("submit",i=>{i.preventDefault();const o=c.value.trim();u.innerHTML="",c.value="",n.style.display="block";const a=new URLSearchParams({key:"41494285-2be0c6d487dc7750955372a82",q:o,image_type:"photo",orientation:"horizontal",safesearch:"true"});fetch(`https://pixabay.com/api/?${a}`).then(t=>{if(n.style.display="none",!t.ok)throw new Error(t.status);return t.json()}).then(t=>{if(t.hits.length===0)throw l.show({message:"Sorry, there are no images matching your search query. Please try again!",theme:"dark",backgroundColor:"#EF4040",titleColor:"white",position:"topRight"});const e=t.hits.reduce((s,{webformatURL:p,largeImageURL:d,tags:m,likes:y,views:f,comments:h,downloads:g})=>s+`<li class="gallery-item">
+          <a class="gallery-link" href="${d}">
            <img class="gallery-image"
-           src="${c}"
+           src="${p}"
            alt="${m}"
            />
-          </a>
-          <p>Likes:${p}</p>
-          <p>Views:${d}</p>
-          <p>Comments:${f}</p>
-          <p>Downloads:${y}</p>
-        </li>`,"");r.innerHTML=t,new g("ul.gallery a",{captionDelay:250,captionsData:"alt"}).open()}).catch(e=>{l.error({message:e.message,color:"red",position:"topCenter"})})});
+          </a>          
+          <div class="description">
+          <p>Likes:<span>${y}</span></p>
+          <p>Views:<span>${f}</span></p>
+          <p>Comments:<span>${h}</span></p>
+          <p>Downloads:<span>${g}</span></p>
+          </div> 
+        </li>`,"");u.innerHTML=e,new L("ul.gallery a",{captionDelay:250,captionsData:"alt"}).refresh()}).catch(t=>{n.style.display="none",l.error({message:t.message,color:"red",position:"topCenter"}),console.error("Error fetching data:",t)})});
 //# sourceMappingURL=commonHelpers.js.map
